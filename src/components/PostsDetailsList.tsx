92d8d1ffc2bd243ccc3cee55de9b2887e246c2fe
Ivan <i.zhuravlev@conteq.ru>
2021-04-27 16:35:27 +0300

    styles width fixed

--- src/components/PostsDetailsList.tsx
+++ src/components/PostsDetailsList.tsx
@@ -2,46 +2,32 @@ import React from 'react';
 import { DetailsList } from '@fluentui/react';
 import { IPost } from '../types/types';
 
-
-  
-  const operations = [
-    {
-      from: '0000 0284 7529 4751 8475',
-      to: '0980 9808 8200 7680',
-      amount: '$1 510',
-      date: '20-05-2020',
-    },
-    {
-      from: '0000 0284 7529 4751 8475',
-      to: '0980 9808 8200 7680',
-      amount: '$1 010',
-      date: '19-05-2020',
-    },
-    {
-      from: '0000 0284 7529 4751 8475',
-      to: '0980 9808 8200 7680',
-      amount: '$1 350',
-      date: '18-05-2020',
-    },
-    {
-      from: '0000 0284 7529 4751 8475',
-      to: '0980 9808 8200 7680',
-      amount: '$1 900',
-      date: '18-05-2020',
-    },
-    {
-      from: '0000 0284 7529 4751 8475',
-      to: '0980 9808 8200 7680',
-      amount: '$6 350',
-      date: '17-05-2020',
-    }
-  ]
   
   const columns = [
-    { key: 'column1', name: 'userId', fieldName: 'userId', minWidth: 100, maxWidth: 300, isResizable: true },
-    { key: 'column2', name: 'id', fieldName: 'id', minWidth: 100, maxWidth: 300, isResizable: true },
-    { key: 'column3', name: 'title', fieldName: 'title', minWidth: 100, maxWidth: 300, isResizable: true },
-    { key: 'column4', name: 'body', fieldName: 'body', minWidth: 100, maxWidth: 300, isResizable: true },
+    { key: 'column1', 
+    name: 'userId', 
+    fieldName: 'userId', 
+    minWidth: 100, 
+    maxWidth: 100, 
+    isResizable: true },
+    { key: 'column2', 
+    name: 'id', 
+    fieldName: 'id', 
+    minWidth: 100, 
+    maxWidth: 100, 
+    isResizable: true },
+    { key: 'column3', 
+    name: 'title', 
+    fieldName: 'title', 
+    minWidth: 100, 
+    maxWidth: 300, 
+    isResizable: true },
+    { key: 'column4', 
+    name: 'body', 
+    fieldName: 'body', 
+    minWidth: 100, 
+    maxWidth: 300, 
+    isResizable: true },
   ]
 
 
