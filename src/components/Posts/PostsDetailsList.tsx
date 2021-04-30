8671617e81cff57ff5ce84381faafff2b6e476bd
Ivan <i.zhuravlev@conteq.ru>
2021-04-30 13:45:29 +0300

    Tabs

--- src/components/Posts/PostsDetailsList.tsx
+++ src/components/Posts/PostsDetailsList.tsx
@@ -8,6 +8,7 @@ import { Dialog } from "@fluentui/react/lib/Dialog";
 import { columns } from "../../styles/columns";
 import { ProgressIndicator } from "@fluentui/react/lib/ProgressIndicator";
 import ReactPaginate from "react-paginate";
+import "./../../styles/TabsPostsStyle.css"
 import style from "../../styles/ProfileStyle.module.css";
 
 /* получаем выбранного пользователя */
@@ -46,61 +47,133 @@ const PostsDetailsList: FC<PostsListProps> = ({ posts }) => {
     [labelId, subTextId]
   );
 
+  /* Табы */
+
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
   return (
     <div>
-      {/* Прелоадер */}
-      {posts.length === 0 ? (
-        <ProgressIndicator label="Loading" />
-      ) : (
-        <div data-is-scrollable={true}>
-          <div>
-            <div>
-              <DefaultButton
-                secondaryText="Открыть профиль"
-                onClick={toggleHideDialog}
-                text="Открыть профиль"
-              />
-            </div>
-            <>
-              <Dialog
-                hidden={hideDialog}
-                onDismiss={toggleHideDialog}
-                modalProps={modalProps}
-              >
-                {pickedUser.length === 0 ? (
-                  <div>Пожалуйста, выберите сообщение</div>
-                ) : (
-                  <PickedUserForm pickedUser={pickedUser} />
-                )}
-              </Dialog>
-            </>
+      <div>
+        <div className="container">
+          <div className="bloc-tabs">
+            <button
+              className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
+              onClick={() => toggleTab(1)}
+            >
+              Tab 1
+            </button>
+            <button
+              className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
+              onClick={() => toggleTab(2)}
+            >
+              Tab 2
+            </button>
+            <button
+              className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
+              onClick={() => toggleTab(3)}
+            >
+              Tab 3
+            </button>
           </div>
-          <div>
-            <DetailsList
-              items={displayPosts}
-              columns={columns}
-              selectionPreservedOnEmptyClick={true}
-              ariaLabelForSelectionColumn="Toggle selection"
-              ariaLabelForSelectAllCheckbox="Toggle selection for all items"
-              checkButtonAriaLabel="select row"
-              onItemInvoked={PickedUser}
-            />
-            <div>
-              <ReactPaginate
-                previousLabel={"Previous"}
-                netLabel={"Next"}
-                pageCount={pageCount}
-                onPageChange={changePage}
-                containerClassName={style.paginationBttns}
-                previousLinkClassName={"previousBttn"}
-                nextLinkClassname={"nextBttn"}
-                disableClassname={"paginationDisabled"}
-                activeClassName={"paginationActive"}
-              />
+
+          <div className="content-tabs">
+            <div
+              className={
+                toggleState === 1 ? "content  active-content" : "content"
+              }
+            >
+              {/* Таб 1 */}
+              {/* Прелоадер */}
+              {posts.length === 0 ? (
+                <ProgressIndicator label="Loading" />
+              ) : (
+                <div data-is-scrollable={true}>
+                  <div>
+                    <div>
+                      <DefaultButton
+                        secondaryText="Открыть профиль"
+                        onClick={toggleHideDialog}
+                        text="Открыть профиль"
+                      />
+                    </div>
+                    <>
+                      <Dialog
+                        hidden={hideDialog}
+                        onDismiss={toggleHideDialog}
+                        modalProps={modalProps}
+                      >
+                        {pickedUser.length === 0 ? (
+                          <div>Пожалуйста, выберите сообщение</div>
+                        ) : (
+                          <PickedUserForm pickedUser={pickedUser} />
+                        )}
+                      </Dialog>
+                    </>
+                  </div>
+                  <div>
+                    <DetailsList
+                      items={displayPosts}
+                      columns={columns}
+                      selectionPreservedOnEmptyClick={true}
+                      ariaLabelForSelectionColumn="Toggle selection"
+                      ariaLabelForSelectAllCheckbox="Toggle selection for all items"
+                      checkButtonAriaLabel="select row"
+                      onItemInvoked={PickedUser}
+                    />
+                    <div>
+                      <ReactPaginate
+                        previousLabel={"Previous"}
+                        netLabel={"Next"}
+                        pageCount={pageCount}
+                        onPageChange={changePage}
+                        containerClassName={style.paginationBttns}
+                        previousLinkClassName={"previousBttn"}
+                        nextLinkClassname={"nextBttn"}
+                        disableClassname={"paginationDisabled"}
+                        activeClassName={"paginationActive"}
+                      />
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+
+            <div
+              className={
+                toggleState === 2 ? "content  active-content" : "content"
+              }
+            >
+              <h2>Content 2</h2>
+              <hr />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Sapiente voluptatum qui adipisci.
+              </p>
+            </div>
+
+            <div
+              className={
+                toggleState === 3 ? "content  active-content" : "content"
+              }
+            >
+              <h2>Content 3</h2>
+              <hr />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos
+                sed nostrum rerum laudantium totam unde adipisci incidunt modi
+                alias! Accusamus in quia odit aspernatur provident et ad vel
+                distinctio recusandae totam quidem repudiandae omnis veritatis
+                nostrum laboriosam architecto optio rem, dignissimos voluptatum
+                beatae aperiam voluptatem atque. Beatae rerum dolores sunt.
+              </p>
             </div>
           </div>
         </div>
-      )}
+      </div>
     </div>
   );
 };
